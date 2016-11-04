5f92a40ef02fe03809753d8062538b912305ee22
foxsugar <redalert3@126.com>
2016-11-04 09:38:28 +0800

    test

--- src/main/java/server/TestOkHttp.java
+++ src/main/java/server/TestOkHttp.java
@@ -1,19 +1,27 @@
 package server;
 
-import okhttp3.CacheControl;
-import okhttp3.Cache;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import okhttp3.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by SunXianping on 2016/7/29 0029.
  */
 public class TestOkHttp {
     static OkHttpClient client = new OkHttpClient();
+    static OkHttpClient client1 = new OkHttpClient();
+    static Gson gson = new Gson();
 
 
     public void test() throws IOException {
@@ -61,6 +69,80 @@ public class TestOkHttp {
     }
 
 
+
+    private String getCid() throws IOException {
+        Request request = new Request.Builder().url("http://localhost:5000/?type=cid").build();
+        Response response = client1.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            return null;
+        }
+    }
+
+    private String getPwd(String pwd) throws IOException {
+        Request request = new Request.Builder().url("http://localhost:5000/?type=pwd&param="+pwd).build();
+        Response response = client1.newCall(request).execute();
+        if (response.isSuccessful()) {
+            String result = response.body().string();
+            System.out.println(result);
+
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    private void login(String telNum,String pwd) throws IOException {
+        //cid
+        String cid = getCid();
+        //pwd
+        String ccPasswd = getPwd(pwd);
+        Map<String, Object> rootMap = new HashMap<>();
+        rootMap.put("cid",cid);
+        rootMap.put("cv","2.3.0");
+        rootMap.put("en","3");
+        rootMap.put("sn","EVA-TL00");
+        rootMap.put("sp","1080x1812");
+        rootMap.put("st","1");
+        rootMap.put("sv","6.0");
+        rootMap.put("t","");
+
+        Map<String, String> reqMap = new HashMap<>();
+        reqMap.put("cellNum",telNum);
+        reqMap.put("sendSmsFlag","1");
+        reqMap.put("ccPasswd",ccPasswd);
+
+        rootMap.put("reqBody", reqMap);
+
+
+        //to json
+        String json = gson.toJson(rootMap);
+
+        RequestBody requestBody = RequestBody.create(MediaType.parse("JSON"), json);
+
+
+        Request request = new Request.Builder().url("https://clientaccess.10086.cn/biz-orange/LN/uamlogin/login")
+                .addHeader("Accept-Encoding", "gzip")
+                .addHeader("Content-Type", "application/Json")
+                .addHeader("Connection", "Keep-Alive")
+                .addHeader("Content-Encoding", "UTF-8")
+                .post(requestBody)
+                .build();
+
+        System.out.println(request.toString());
+
+        Response response = client1.newCall(request).execute();
+//        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+//        byte[] b = response.body().bytes(); //获取数据的bytes
+//        String info = new String(b, "utf-8");
+//        System.out.println("Response  response:          " + info);
+//        Map map = gson.fromJson(response.body().charStream(),new TypeToken<HashMap>(){}.getType());
+        System.out.println(response.body().string());
+//        System.out.println(response.body().string());
+
+    }
+
     public static void main(String[] args) {
 
         int cacheSize = 10 * 1024 * 1024; // 10 MiB
@@ -72,9 +154,13 @@ public class TestOkHttp {
                 .build();
 
 
+        client1 = new OkHttpClient().newBuilder()
+                .build();
+
+
         TestOkHttp test = new TestOkHttp();
         try {
-            test.test();
+            test.login("13821033039","102426");
         } catch (IOException e) {
             e.printStackTrace();
         }
